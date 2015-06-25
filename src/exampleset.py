d9849f54be4648737f390dfc280bf1a916c5b70d
Diogo Haruki <dkykuta@gmail.com>
2015-06-25 19:37:09 -0300

    refactoring done.

---
+++ src/exampleset.py
@@ -0,0 +1,55 @@
+#! \usr\bin\python
+# -*- coding: utf-8 -*-
+#
+#
+
+from sklearn import svm
+
+class ExampleSet:
+    def __init__(self):
+        self.data = []
+        self.label = []
+
+    def __len__(self):
+        return len(self.data)
+
+    def add_info(self, data, label):
+        self.data.append(data)
+        self.label.append(label)
+
+    def shuffle(self):
+        aux = zip(self.data, self.label)
+        import random
+        random.shuffle(aux)
+        self.data = [x for x,y in aux]
+        self.label = [y for x,y in aux]
+
+    def crossvalidation(self, fold=10):
+        N = len(self) 
+        step = N/fold
+
+        best = (2, -1, -1, "rbf")
+        # encontra o melhor conjunto de parametros
+        for kernel, gamma, C in [(k, g, c) for k in ["linear", "rbf"] for g in [0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001, 0.000001] for c in [1., 10., 100., 1000.]]:
+            acertos = 0
+            for i in xrange(fold+1):
+                inf, sup = i * step, min((i+1) * step, N)
+
+                traindataset = self.data[0:inf] + self.data[sup:N]
+                trainlabelset = self.label[0:inf] + self.label[sup:N]
+
+                validationdataset = self.data[inf:sup]
+                validationlabelset = self.label[inf:sup]
+
+                classifier = svm.SVC(kernel=kernel, gamma=gamma, C=C)
+                classifier.fit(traindataset, trainlabelset)
+
+                ans = classifier.predict(validationdataset)
+                acertos += len([x for x in (ans == validationlabelset) if x == True])
+            error = float(N - acertos) / N
+
+            if error < best[0]:
+                best = (error, gamma, C, kernel)
+            print "[kernel: %s gamma: %5s C: %5s] %s" % (kernel, gamma, C, error)
+
+        return best
--- src/main.py
+++ src/main.py
@@ -1,15 +1,23 @@
 #! \usr\bin\python
 # -*- coding: utf-8 -*-
 
+import os
+import cv2
+import math
+import pickle
+import shutil
+from sklearn import svm
+
 import utils
+import imgfun
 from step1 import extract_plate
 from step2 import to_binary
 from step3 import extract_last_digit
-from step4 import recognize_digit
-import imgfun
+from step4 import make_features_array, recognize_digit
+from exampleset import ExampleSet
 
-if __name__ == "__main__":
-    img = utils.open_gray_image_as_np("../images/img_001.jpg")
+def classify(fpath):
+    img = utils.open_gray_image_as_np(fpath)
 
     plate = extract_plate(img)
     imgfun.save_debug_image(plate, 'plate')
@@ -23,3 +31,60 @@ if __name__ == "__main__":
     digit = recognize_digit(last_digit_img)
 
     print "O ultimo digito dessa placa e %s" % (digit)
+
+def train_digits():
+    digitsdir = '../digits'
+    outdir = '../learned'
+    outfile = '%s/digits-train' % outdir
+
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+
+    if os.path.exists(outfile):
+        os.remove(outfile)
+
+    fullset = ExampleSet()
+
+    for d in xrange(0,10):
+        currdir = os.path.join(digitsdir, "%d" % d)
+        files = os.listdir(currdir)
+        for f in files:
+            img = utils.open_gray_image_as_np(os.path.join(currdir, f))
+            fullset.add_info(make_features_array(img), "%d" % d)
+
+    # essa porcaria ta muito concentrado
+    fullset.shuffle()
+    # crossvalidation
+    error, gamma, C, kernel = fullset.crossvalidation(10)
+    print error, gamma, C, kernel
+
+    # salva o classificador escolhido como melhor
+    clf = svm.SVC(kernel = kernel, gamma = gamma, C=C)
+    clf.fit(fullset.data, fullset.label)
+    clf_file = open(outfile, "w")
+    pickle.dump(clf, clf_file)
+    clf_file.close()
+
+
+
+if __name__ == "__main__":
+    import sys
+    print len(sys.argv), sys.argv
+    if len(sys.argv) < 2:
+        print "What do you want?\nUsage: ..."
+        sys.exit(0)
+    else:
+        for arg in sys.argv[1:]:
+            if arg == '-td':
+                print "Training digits (step 4)"
+                train_digits()
+                sys.exit(0)
+            elif arg == '-tp':
+                print "Training plates (step 1)"
+                sys.exit(0)
+
+    print "I think you want to recognize the last digit from the plate in this picture '%s', am I correct? [Y/n]" % sys.argv[1]
+    classify("../images/img_001.jpg")
+    classify("../images/img_001.jpg")
+    classify("../images/img_001.jpg")
+    classify("../images/img_001.jpg")
